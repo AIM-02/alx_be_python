66506094d144fca93c6dc81fde452a9a2f560b3e
maryam <maryamismaila97@gmail.com>
2024-09-22 21:18:44 +0100

    a script that calculate simple interest

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,10 @@
+# Assign values to variables
+principal = 1000    #representing $1000
+rate = 0.05          # representing 5% annual interest rate
+time = 3             # representing 3 years
+
+# calculate simple interest
+interest = principal * rate * time
+
+#output
+print(f"The simple interest is: {interest}")
