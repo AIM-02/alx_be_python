9fbfa34258b842c9fcbc3bfd5807e05a58ec2db5
maryam <maryamismaila97@gmail.com>
2024-09-22 20:51:25 +0100

    created a python script that execute basic arithmetic operation

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,15 @@
+# define the numbers
+number1 = 10
+number2 = 5
+
+# perform arithmetic operation addition,substraction and multiplication on this numbers
+sum_of_numbers=number1 + number2
+difference_of_numbers= number1 - number2
+product_of_numbers= number1 * number2
+
+#print out result of the operations
+print(f"The Addition of {number1} and {number2} is {sum_of_numbers}")
+print(f"The Difference of {number1} and {number2} is {difference_of_numbers}")
+print(f"The Product of {number1} and {number2} is {product_of_numbers}")
+
+
